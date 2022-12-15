111de8bacb425107eef26e87acd08c7772997528
SOUISSI-Mohamed <mohamedsouissi2203@gmail.com>
2022-12-15 19:27:16 +0100

    structure

---
+++ imc.js
@@ -0,0 +1,14 @@
+var header = $('<div> hello </div>')
+var imgBG =$('<img src='media/mainbg.png' alt='main background image>')
+$('body').append(header)
+$('div').append(img)
+
+
+
+$('img').css({"height": "100%",
+  "background-position": "center",
+  "background-repeat": "no-repeat",
+  "background-size": "cover"})
+
+
+
